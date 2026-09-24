20269c7aee6d36189108fbcbc23bc5b78bf6312c
Firas14613 <lulufiras9@gmail.com>
2026-09-24 08:55:22 +0700

    Perbaikan Kalkulator pratikum 02

--- praktikum/02-bahasa-pemrograman-go/unguided/kalkulator/kalkutalor.go
+++ praktikum/02-bahasa-pemrograman-go/unguided/kalkulator/kalkutalor.go
@@ -8,15 +8,33 @@ func main() {
 		b int
 	)
 
-	fmt.Scanln(&a)
-	fmt.Scanln(&b)
+	for {
+		fmt.Scanln(&a)
+		fmt.Scanln(&b)
 
-	tambah := a+b
-	kurang := a-b
-	kali := a*b
-	bagi := a/b
-	sisa := a%b
+		if b == 0 {
+			fmt.Println("Input Ulang")
+			continue
+		}
 
-	fmt.Print(tambah, " ", kurang, " ", kali, " ", bagi, " ", sisa)
+		fmt.Println(a+b, " ", a-b, " ", a*b, " ", a/b, " ", a%b)
+		break
+	}
+	// tambah := a+b
+	// kurang := a-b
+	// kali := a*b
+	// bagi := a/b
+	// sisa := a%b
+
+	// if b==0 {
+	// 	fmt.Println("Input Ulang")
+	// }
+
+	// fmt.Println(a+b)
+	// fmt.Println(a-b)
+	// fmt.Println(a*b)
+	// fmt.Println(a/b)
+	// fmt.Println(a%b)
+	// fmt.Println(a+b, " ", a-b, " ", a*b, " ", a/b, " ", a%b)
 
 }
